a77025dbacf4267831bcbfc95a3afbed0218e2d9
PC <fernandezfrancoa2001@gmail.com>
2024-04-27 18:43:34 -0300

    first commit

---
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -0,0 +1,60 @@
+package com.bolsadeideas.springboot.web.app.controllers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bolsadeideas.springboot.web.app.models.Usuario;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/app")
+public class IndexController {
+
+	@GetMapping({ "/index", "/", "", "/home" })
+	public String index(Model model) {
+		model.addAttribute("titulo", "Hola spring Framework!");
+		return "index";
+	}
+
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Denise");
+		usuario.setApellido("Monges");
+		usuario.setEmail("denmon@prueba.com");
+
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+
+		return "perfil";
+	}
+
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+
+		model.addAttribute("titulo", "Listado de usuarios: ");
+
+		return "listar";
+	}
+	
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
+		List<Usuario> usuarios = Arrays.asList(new Usuario("Denise", "Monges", "denmon@prueba.com"),
+				new Usuario("Franco", "Fernandez", "franmon@prueba.com"),
+				new Usuario("pickachu", "thompson", "pimon@prueba.com"),
+				new Usuario("Barry", "Allen", "flashmon@prueba.com"));
+		
+		return usuarios;
+		
+	}
+
+}
