1c84c5f169b764a6b702fe173bf7f006f2d52fd6
PC <fernandezfrancoa2001@gmail.com>
2024-05-04 02:23:03 -0300

    inyeccion-dependencias

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controller/IndexController.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.di.app.controller;
 
+import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    private MiServicio servicio = new MiServicio();
+
+
     @GetMapping({ "/", "", "/index"})
     public String index(Model model) {
+
+        model.addAttribute("objeto", servicio.operacion()); // se pasa el objeto a la vista
+
         return "index";
     }
 }
