fa8a7d5acf9b545a6e6b6ad7d1c5df7e9fc309e1
PC <fernandezfrancoa2001@gmail.com>
2024-05-07 02:24:40 -0300

    inyeccion-dependencias

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controller/IndexController.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.bolsadeideas.springboot.di.app.controller;
 
 import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
