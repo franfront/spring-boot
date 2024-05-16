7ad4601d36a7a19fcaf3397ec6f04f16143aa6e7
PC <fernandezfrancoa2001@gmail.com>
2024-05-16 17:08:52 -0300

    inyeccion-dependencias

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicioComplejo.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicioComplejo.java
@@ -3,8 +3,8 @@ package com.bolsadeideas.springboot.di.app.models.service;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Component("miServicioComplejo")
-@Primary // se le indica a Spring que este es el bean principal
+//@Component("miServicioComplejo")
+//@Primary // se le indica a Spring que este es el bean principal
 public class MiServicioComplejo implements IServicio{
 
     @Override
