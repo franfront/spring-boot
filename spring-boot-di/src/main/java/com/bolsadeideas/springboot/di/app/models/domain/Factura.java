0711bd74d1441d0e261ba7c99ca4b13726e21925
PC <fernandezfrancoa2001@gmail.com>
2024-05-31 00:16:27 -0300

    inyeccion-dependencias

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
@@ -3,14 +3,23 @@ package com.bolsadeideas.springboot.di.app.models.domain;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.io.Serializable;
 import java.util.List;
 
 @Component
-public class Factura {
+@RequestScope
+public class Factura implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
 
     @Value("${factura.descripcion}")
     private String descripcion;
