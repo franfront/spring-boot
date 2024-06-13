01a28282d189a4653668c83621c7bb1228105a86
PC <fernandezfrancoa2001@gmail.com>
2024-06-13 11:53:19 -0300

    formularios

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
@@ -14,7 +15,15 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String procesar(Model model) {
+    public String procesar(Model model,
+                           @RequestParam(name="username") String username,
+                           @RequestParam String password,
+                           @RequestParam String email) {
+
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        model.addAttribute("email", email);
+
         return "resultado";
     }
 
