de3bbe5403f2d84a2b7daddf979f4274f0bbe266
PC <fernandezfrancoa2001@gmail.com>
2024-07-29 14:01:02 -0300

    m

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
+import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ public class FormController {
 
     @GetMapping("/form")
     public String form(Model model) {
+        model.addAttribute("titulo", "Formulario usuarios");
         return "form";
     }
 
@@ -20,9 +22,14 @@ public class FormController {
                            @RequestParam String password,
                            @RequestParam String email) {
 
+        Usuario usuario = new Usuario();
+        usuario.setUsername(username);
+        usuario.setPassword(password);
+        usuario.setEmail(email);
+
         model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        model.addAttribute("email", email);
+        model.addAttribute("titulo", "Resultado form");
+        model.addAttribute("usuario", usuario);
 
         return "resultado";
     }
