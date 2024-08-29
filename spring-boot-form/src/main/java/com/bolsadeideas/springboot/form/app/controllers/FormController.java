ea05d8b98dda8587cbd5f176de95d1e7ae054068
PC <fernandezfrancoa2001@gmail.com>
2024-08-29 02:37:42 -0300

    mvalidaciones

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,11 +1,17 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class FormController {
@@ -17,17 +23,22 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String procesar(Model model,
-                           @RequestParam(name="username") String username,
-                           @RequestParam String password,
-                           @RequestParam String email) {
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
 
-        Usuario usuario = new Usuario();
-        usuario.setUsername(username);
-        usuario.setPassword(password);
-        usuario.setEmail(email);
+        model.addAttribute("titulo", "Resultado form");
+        if(result.hasErrors()){
 
-        model.addAttribute("username", username);
+            Map<String, String> errores = new HashMap<>();
+
+            result.getFieldErrors().forEach(err -> {
+                errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+            });
+
+            model.addAttribute("error", errores);
+
+            return "form";
+        }
+
         model.addAttribute("titulo", "Resultado form");
         model.addAttribute("usuario", usuario);
 
